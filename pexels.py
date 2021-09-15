d16415ac89d4f514270a6060e991edf1a9eeb3b2
seffu <seffukioi@gmail.com>
2021-09-15 07:45:43 +0300

    get two keywords

--- pexels.py
+++ pexels.py
@@ -6,18 +6,21 @@ def pexels():
     key_words = [
         'man', 'woman','old man','old woman','baby girl','baby boy','teenage girl','teenage boy'
         ]
-    #i is the number of the keyword you want to run eg 0 = man
-    i = 0
+    i = 6
     page = 1
     while True:
-        url = f"https://api.pexels.com/v1/search?query={key_words[i]}&per_page=80&page={page}"
-        headers = CaseInsensitiveDict()
-        #Replace [YOUR_API_KEY] with the key that pexels generates for you.
-        headers["Authorization"] = "Bearer [YOUR_API_KEY]"
-        resp = requests.get(url, headers=headers)
-        our_json = json.loads(resp.text)
-        with open(f'{key_words[i]}.txt', 'a') as pt:
-            pt.write("%s\n" % our_json)
-        page+=1
-    print(page)
+        count = 1
+        for reqs in range(200):
+            url = f"https://api.pexels.com/v1/search?query={key_words[i]}&per_page=80&page={page}"
+            headers = CaseInsensitiveDict()
+            headers["Authorization"] = "Bearer 563492ad6f9170000100000152b68c48108449c694c5e2b5571ce90a"
+            resp = requests.get(url, headers=headers)
+            our_json = json.loads(resp.text)
+            with open(f'{key_words[i]}.txt', 'a') as pt:
+                pt.write("%s\n" % our_json)
+            page+=1
+            count += 1
+            if count == 101:
+                page = 1
+                i +=1
 pexels()
